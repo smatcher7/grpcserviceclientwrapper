c209a85c082e262594e24df8338086106a34ad75
Rui Ferreira <ruiferreira91@gmail.com>
2024-11-24 19:46:27 +0000

    minor tweaks on service extensions

--- GrpcService.Client.Wrapper/ServicesExtensions.cs
+++ GrpcService.Client.Wrapper/ServicesExtensions.cs
@@ -1,5 +1,7 @@
-﻿using Grpc.Core;
+﻿using System.Globalization;
+using Grpc.Core;
 using Grpc.Core.Interceptors;
+using Grpc.Net.Client.Web;
 using Grpc.Net.ClientFactory;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,81 +9,77 @@ namespace GrpcService.Client.Wrapper;
 
 public static class ServicesExtensions
 {
-    public static IServiceCollection AddGrpcClient<TClient>(this IServiceCollection services, string endpoint) where TClient : ClientBase
+    public static IServiceCollection AddGrpcClient<TClient>(this IServiceCollection services, Action<WrapperOptions> optionsSetup) where TClient : ClientBase
     {
-        services.AddGrpcClient<TClient>(endpoint, false, null);
+        WrapperOptions options = new WrapperOptions("https://localhost:443");
+
+        optionsSetup?.Invoke(options);
+
+        services.AddGrpcClient<TClient>(options);
         return services;
     }
 
-    public static IServiceCollection AddGrpcClient<TClient, TInterceptor>(this IServiceCollection services, string endpoint)
+    public static IServiceCollection AddGrpcClient<TClient, TInterceptor>(this IServiceCollection services, Action<WrapperOptions> optionsSetup)
         where TClient : ClientBase
         where TInterceptor : Interceptor
     {
-        services.AddGrpcClient<TClient, TInterceptor>(endpoint, false, null);
+        WrapperOptions options = new WrapperOptions("https://localhost:443");
+
+        services.AddGrpcClient<TClient, TInterceptor>(options);
         return services;
     }
 
-    public static IServiceCollection AddAuthorizedGrpcClient<TClient>(this IServiceCollection services, string endpoint, string authToken)
-        where TClient : ClientBase
+    private static void AddGrpcClient<TClient>(this IServiceCollection services, WrapperOptions options) where TClient : ClientBase
     {
-        services.AddGrpcClient<TClient>(endpoint, true, authToken);
-        return services;
+        GenerateHttpClientBuilder<TClient>(services, options);
     }
 
-    public static IServiceCollection AddAuthorizedGrpcClient<TClient, TInterceptor>(this IServiceCollection services, string endpoint, string authToken)
-        where TClient : ClientBase
+    private static void AddGrpcClient<TClient, TInterceptor>(this IServiceCollection services, WrapperOptions options) where TClient : ClientBase
         where TInterceptor : Interceptor
     {
-        services.AddGrpcClient<TClient, TInterceptor>(endpoint, true, authToken);
-        return services;
+        GenerateHttpClientBuilder<TClient>(services, options).AddInterceptor<TInterceptor>(InterceptorScope.Client);
     }
 
-    private static void AddGrpcClient<TClient>(this IServiceCollection services,
-        string endpoint,
-        bool useAuthentication,
-        string? authToken = null)
-        where TClient : ClientBase
+    private static IHttpClientBuilder GenerateHttpClientBuilder<TClient>(IServiceCollection services, WrapperOptions options) where TClient : ClientBase
     {
-        var httpClientBuilder = services.AddGrpcClient<TClient>(o => { o.Address = new Uri(endpoint); })
+        var httpClientBuilder = services.AddGrpcClient<TClient>(o => { o.Address = new Uri(options.ServiceEndpoint); })
             .ConfigurePrimaryHttpMessageHandler(() =>
             {
-                var handler = new HttpClientHandler();
+                HttpMessageHandler? handler = null;
+
+                var httpHandler = new HttpClientHandler
+                {
+                    SslProtocols = options.SslProtocol
+                };
+                if (options.IgnoreCertificates)
+                {
+                    httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+                }
+
+                if (options.UseGrpcWeb)
+                {
+                    handler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, httpHandler);
+                }
+                else
+                {
+                    handler = httpHandler;
+                }
+
                 return handler;
-            });
-        if (useAuthentication && authToken != null)
-        {
-            httpClientBuilder = httpClientBuilder.AddCallCredentials((context, metadata) =>
+            })
+            .EnableCallContextPropagation(c => c.SuppressContextNotFoundErrors = true)
+            .AddCallCredentials((context, metadata) =>
             {
-                metadata.Add("Authorization", $"Bearer {authToken}");
+                metadata.Add("accept-language", CultureInfo.CurrentCulture.Name);
+
+                if (options.UseAuthentication && options.AuthToken != null)
+                {
+                    metadata.Add("Authorization", $"Bearer {options.AuthToken}");
+                }
+
                 return Task.CompletedTask;
             });
-        }
 
-        httpClientBuilder.EnableCallContextPropagation();
-    }
-
-    private static void AddGrpcClient<TClient, TInterceptor>(this IServiceCollection services,
-        string endpoint,
-        bool useAuthentication,
-        string? authToken = null)
-        where TClient : ClientBase
-        where TInterceptor : Interceptor
-    {
-        var httpClientBuilder = services.AddGrpcClient<TClient>(o => { o.Address = new Uri(endpoint); })
-            .ConfigurePrimaryHttpMessageHandler(() =>
-            {
-                var handler = new HttpClientHandler();
-                return handler;
-            }).AddInterceptor<TInterceptor>(InterceptorScope.Client);
-
-        if (useAuthentication && authToken != null)
-        {
-            httpClientBuilder = httpClientBuilder.AddCallCredentials((context, metadata) =>
-            {
-                metadata.Add("Authorization", $"Bearer {authToken}");
-                return Task.CompletedTask;
-            });
-        }
-        httpClientBuilder.EnableCallContextPropagation();
+        return httpClientBuilder;
     }
 }
---
+++ GrpcService.Client.Wrapper/WrapperOptions.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Authentication;
+
+namespace GrpcService.Client.Wrapper;
+
+public class WrapperOptions
+{
+    public WrapperOptions(string endpoint)
+    {
+        ServiceEndpoint = endpoint;
+    }
+
+    public string ServiceEndpoint { get; set; }
+
+    public SslProtocols SslProtocol { get; set; } = SslProtocols.Tls12;
+
+    public bool IgnoreCertificates { get; set; }
+
+    public bool UseGrpcWeb { get; set; }
+
+    public bool UseAuthentication { get; set; }
+
+    public string? AuthToken { get; set; }
+}
