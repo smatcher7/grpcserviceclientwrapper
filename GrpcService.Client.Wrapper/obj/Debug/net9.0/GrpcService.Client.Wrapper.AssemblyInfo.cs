d535347e376a410c42cb1651c68f1b597f8f8796
Rui Ferreira <ruiferreira91@gmail.com>
2024-12-07 17:32:43 +0000

    commit

--- GrpcService.Client.Wrapper/obj/Debug/net9.0/GrpcService.Client.Wrapper.AssemblyInfo.cs
+++ GrpcService.Client.Wrapper/obj/Debug/net9.0/GrpcService.Client.Wrapper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smatcher™")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.2.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.2+7212fbed1f0d3c20fb671b109fdc656350d22210")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.2+b28f3c3dc1776a5921b8939ba6aed5bdd9c9bb30")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrpcService.Client.Wrapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrpcService.Client.Wrapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.2.0")]
